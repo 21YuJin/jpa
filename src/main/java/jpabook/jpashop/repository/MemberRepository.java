0280e15fce13e5fccc22a5a0e0518c0e6ef5d26a
21YuJin <you5463@ajou.ac.kr>
2024-07-21 18:08:19 +0900

    finish section2

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,4 +1,36 @@
 package jpabook.jpashop.repository;
 
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository // Spring에서 제공하는 @Repository를 사용하면 Component Scan에 의해서 자동으로 Spring 빈으로 관리가 됨
+@RequiredArgsConstructor
 public class MemberRepository {
+
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // 첫 번째 type, 두 번째 PK
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    // parameter binding
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name =:name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
 }
